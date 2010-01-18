f252d9c39b75d65eb002b76a7f1f8111b9cfc7e8
Joshua Hull <joshbuddy@gmail.com>
2010-01-17 19:15:32 -0500

    added node.js interface

---
+++ exmaples/node-example.js
@@ -0,0 +1,15 @@
+require('../lib/sherpa')
+
+var sys = require("sys"), http = require("http");
+var sherpa = new Sherpa.interfaces.NodeJs({
+  '/hello': {to: function (request, response) {
+    response.sendHeader(200, {"Content-Type": "text/plain"});
+    response.sendBody("Hello World\n");
+    response.finish();
+  }}
+});
+
+http.createServer(sherpa.listener()).listen(8000);
+sys.puts("Server running at http://127.0.0.1:8000/");
+
+
