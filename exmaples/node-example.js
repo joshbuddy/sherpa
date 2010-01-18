418c16391764a266e1a65c792d24a4ff0bafc6e6
Joshua Hull <joshbuddy@gmail.com>
2010-01-17 19:31:23 -0500

    renamed var.

--- exmaples/node-example.js
+++ exmaples/node-example.js
@@ -1,7 +1,8 @@
 require('../lib/sherpa')
 
 var sys = require("sys"), http = require("http");
-var sherpa = new Sherpa.interfaces.NodeJs({
+
+http.createServer(new Sherpa.interfaces.NodeJs({
   '/hello': {to: function (request, response) {
     response.sendHeader(200, {"Content-Type": "text/plain"});
     response.sendBody("Hello World\n");
@@ -9,7 +10,7 @@ var sherpa = new Sherpa.interfaces.NodeJs({
   }},
   '/hello/:name': {to: function (request, response) {
     response.sendHeader(200, {"Content-Type": "text/plain"});
-    response.sendBody("Hello "+request.sherpa_response.params['name']+"\n");
+    response.sendBody("Hello "+request.sherpaResponse.params['name']+"\n");
     response.finish();
   }},
   'not found': {to: function (request, response) {
@@ -18,9 +19,7 @@ var sherpa = new Sherpa.interfaces.NodeJs({
     response.finish();
   }}
   
-});
-
-http.createServer(sherpa.listener()).listen(8000);
+}).listener()).listen(8000);
 sys.puts("Server running at http://127.0.0.1:8000/");
 
 
