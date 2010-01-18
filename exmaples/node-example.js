7f0ce22a4c5b19e76f11c780df623a6709df9987
Joshua Hull <joshbuddy@gmail.com>
2010-01-17 19:19:31 -0500

    nicer node example and 'not found' support

--- exmaples/node-example.js
+++ exmaples/node-example.js
@@ -6,7 +6,18 @@ var sherpa = new Sherpa.interfaces.NodeJs({
     response.sendHeader(200, {"Content-Type": "text/plain"});
     response.sendBody("Hello World\n");
     response.finish();
+  }},
+  '/hello/:name': {to: function (request, response) {
+    response.sendHeader(200, {"Content-Type": "text/plain"});
+    response.sendBody("Hello "+request.sherpa_response.params['name']+"\n");
+    response.finish();
+  }},
+  'not found': {to: function (request, response) {
+    response.sendHeader(404, {"Content-Type": "text/plain"});
+    response.sendBody("I can't find what you're looking for..");
+    response.finish();
   }}
+  
 });
 
 http.createServer(sherpa.listener()).listen(8000);
