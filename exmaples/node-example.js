d44317e7b4bdd6dcae2ea0c8f7eaf760e4ea29ec
Joshua Hull <joshbuddy@gmail.com>
2010-07-01 14:27:10 -0400

    fixed node example

--- exmaples/node-example.js
+++ exmaples/node-example.js
@@ -1,32 +1,32 @@
-require('../lib/sherpa')
+require('../lib/sherpa/nodejs')
 
 var sys = require("sys"), http = require("http");
 
-http.createServer(new Sherpa.interfaces.NodeJs([
+http.createServer(new Sherpa.NodeJs([
   ['/hello', function (request, response) {
-    response.sendHeader(200, {"Content-Type": "text/plain"});
-    response.sendBody("Hello World\n");
-    response.finish();
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World\n");
+    response.close();
   }],
-  ['/hello/:name', {matchesWith: {name: /^\d+$/}}, function (request, response) {
-    response.sendHeader(200, {"Content-Type": "text/plain"});
-    response.sendBody("Hello Number "+request.sherpaResponse.params['name']+"\n");
-    response.finish();
+  ['/hello/:name', {matchesWith: {name: /\d+/}}, function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Number "+request.sherpaResponse.params['name']+"\n");
+    response.close();
   }],
   ['/hello/:name', function (request, response) {
-    response.sendHeader(200, {"Content-Type": "text/plain"});
-    response.sendBody("Hello "+request.sherpaResponse.params['name']+"\n");
-    response.finish();
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello "+request.sherpaResponse.params['name']+"\n");
+    response.close();
   }],
   ['/hello/:name', {conditions:{method: 'POST'}}, function (request, response) {
-    response.sendHeader(200, {"Content-Type": "text/plain"});
-    response.sendBody("Hello POSTY "+request.sherpaResponse.params['name']+"\n");
-    response.finish();
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello POSTY "+request.sherpaResponse.params['name']+"\n");
+    response.close();
   }],
   ['not found', function (request, response) {
-    response.sendHeader(404, {"Content-Type": "text/plain"});
-    response.sendBody("I can't find what you're looking for..");
-    response.finish();
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("I can't find what you're looking for..");
+    response.close();
   }]
 
 ]).listener()).listen(8000);
